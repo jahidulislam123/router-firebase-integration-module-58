0160538593e5fe123e0b8c055e3c1640ecafe457
unknown <islam15-3879@diu.edu.bd>
2022-04-12 09:32:02 +0600

    added

---
+++ src/Hooks/useFirebase.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react"
+
+const useFirebase=()=>{
+    const [user,setUser]=useState({});
+    useEffect(()=>{
+
+    },[])
+    const signInWithGoogle=()=>{
+        console.log('sign in soon');
+    }
+    return [user,setUser];
+
+}
+export default useFirebase;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,21 +1,18 @@
 import React from 'react';
 
-const Login = () => {
+const Register = () => {
     return (
         <div>
-            <h3>Please Log In</h3>
-        <div style={{margin:'20px'}}>
-            <button>Sign in With google</button>
-
-            </div>
+            <h3>please register now</h3>
             <form action="">
                 <input type="text" name="" placeholder='Your Name' id="" /> <br />
                 <input type="email" name="" placeholder='Your Email' id="" /> <br />
                 <input type="password" name="" placeholder='Password' id="" /> <br />
-                <input type="submit" value="Login" />
+
+                <input type="submit" value="Register" />
             </form>
         </div>
     );
 };
 
-export default Login;
+export default Register;
